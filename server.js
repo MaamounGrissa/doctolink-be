e0316e89258d061aa29664be0cd87a657f40e486
Maamoun <grissa.maamoun@gmail.com>
2023-01-14 14:21:28 +0100

    Cross 5

--- server.js
+++ server.js
@@ -26,22 +26,16 @@ dotenv.config();
 
 app.enable("trust proxy");
 
-/* app.use(
+app.use(
   cors({
     origin: [
-      "https://doctolink-frontend-rust.vercel.app",
-      "https://docto-link.com",
+      "https://doctolink-frontend-rust.vercel.app/",
+      "https://docto-link.com/",
       "https://doctolink-fe.vercel.app/",
-      "http://localhost:3000",
+      "http://localhost:3000/",
     ],
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true,
-  })
-); */
-
-app.use(
-  cors({
-    origin: "*",
   })
 );
 
